ef40dad8e3cef220666150247cc09477e38b71df
Asphähyre <luka.boulagnon@epitech.eu>
2016-07-25 18:25:14 +0200

    Added multi date-ranges is_open check

--- src/is_open.c
+++ src/is_open.c
@@ -3,16 +3,21 @@
 int is_open(opening_hours oh, when date) {
 	if (!oh)
 		return (0);
-	selector_sequence selector = oh->rule.selector;
+	opening_hours cur = oh;
 
-	return ((selector.anyway
+	do {
+		selector_sequence selector = oh->rule.selector;
+
+		if (selector.anyway
 				|| (GET_BIT(selector.wide_range.years, date.tm_year)
 					&& GET_BIT(selector.wide_range.monthdays.days, date.tm_mon * 32 + date.tm_mday - 1)
 					&& ((GET_BIT(selector.small_range.weekday.range, date.tm_wday)
 						&& GET_BIT(selector.small_range.hours.time_range, date.tm_hour * 60 + date.tm_min))
 					|| (GET_BIT(selector.small_range.weekday.range, date.tm_wday - 1 < 0 ? 6 : date.tm_wday - 1)
 						&& GET_BIT(selector.small_range.hours.extended_time_range, date.tm_hour * 60 + date.tm_min)))))
-			&& oh->rule.state.type == RULE_OPEN);
+			return (cur->rule.state.type == RULE_OPEN);
+	} while ((cur = cur->next_item));
+	return (0);
 }
 
 int is_open_time(opening_hours oh, struct tm date) {
